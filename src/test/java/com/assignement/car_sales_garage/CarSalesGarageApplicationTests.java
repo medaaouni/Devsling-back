8e7aa6e2ee1d7c1abe0a984cf73ddf0d20b5e11a
medaaouni <mohamedaaouni2006@gmail.com>
2025-03-28 07:22:44 +0000

    chore: initial project setup

---
+++ src/test/java/com/assignement/car_sales_garage/CarSalesGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignement.car_sales_garage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSalesGarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
