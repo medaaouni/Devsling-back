7181dcb406bb424acdba1a177070550f3cd6b4d2
medaaouni <mohamedaaouni2006@gmail.com>
2025-03-29 14:00:41 +0000

    test: add failing test for filtering cars by fuel and max price - RED

--- src/test/java/com/assignement/car_sales_garage/services/CarServiceTests.java
+++ src/test/java/com/assignement/car_sales_garage/services/CarServiceTests.java
@@ -5,6 +5,7 @@ import com.assignement.car_sales_garage.TestDataUtil;
 import com.assignement.car_sales_garage.domain.dtos.CarRequestDto;
 import com.assignement.car_sales_garage.domain.dtos.CarResponseDto;
 import com.assignement.car_sales_garage.domain.entities.Car;
+import com.assignement.car_sales_garage.enums.FuelType;
 import com.assignement.car_sales_garage.exceptions.InvalidCarException;
 import com.assignement.car_sales_garage.mapper.CarMapper;
 import com.assignement.car_sales_garage.repositories.CarRepository;
@@ -14,6 +15,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -35,7 +38,7 @@ public class CarServiceTests {
     CarMapper carMapper;
 
     @Test
-    public void whenRegisterCarBefore2015__thenRejectCar() {
+    public void whenRegisterCarBefore2015_thenRejectCar() {
 
         CarRequestDto oldCar = TestDataUtil.createCarDtoRequestBefore2015();
 
@@ -45,7 +48,7 @@ public class CarServiceTests {
     }
 
     @Test
-    public void whenRegisterCarAfter2015__thenSaveCar() {
+    public void whenRegisterCarAfter2015_thenSaveCar() {
         CarRequestDto carRequest = TestDataUtil.createCarRequestDto();
         CarResponseDto carResponseDto = TestDataUtil.createCarResponseDto();
         Car expected = TestDataUtil.createCarEntity();
@@ -58,6 +61,16 @@ public class CarServiceTests {
 
     }
 
+
+    @Test
+    public void whenGetCarsByFuelTypeAndMaxPrice_thenReturnFilteredCars() {
+
+        List<Car> expected = List.of(TestDataUtil.createCarEntity());
+
+        when(carRepository.findByFuelTypeAndPriceLessThanEqual(eq(FuelType.DIESEL),eq(15000))).thenReturn(expected);
+
+        assertThat(underTest.getCarsByFuelTypeAndMaxPrice(FuelType.DIESEL,15000)).hasSize(1);
+    }
 
 
 }
