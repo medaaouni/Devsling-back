8e33a80c997c2cd43c742d7fa2af9d01bcfa1490
medaaouni <mohamedaaouni2006@gmail.com>
2025-03-29 01:13:19 +0000

    feat:add unit tests for addCar method - GREEN

--- src/test/java/com/assignement/car_sales_garage/TestDataUtil.java
+++ src/test/java/com/assignement/car_sales_garage/TestDataUtil.java
@@ -1,6 +1,7 @@
 package com.assignement.car_sales_garage;
 
 import com.assignement.car_sales_garage.domain.dtos.CarRequestDto;
+import com.assignement.car_sales_garage.domain.dtos.CarResponseDto;
 import com.assignement.car_sales_garage.domain.entities.Car;
 import com.assignement.car_sales_garage.enums.FuelType;
 import com.assignement.car_sales_garage.enums.Transmission;
@@ -11,9 +12,9 @@ public final class TestDataUtil {
 
     public static Car createCarEntity() {
         return Car.builder()
-                .make("Toyota")
-                .model("Corolla")
-                .registrationDate(LocalDateTime.of(2014, 5, 20, 0, 0))
+                .make("Mercedes")
+                .model("C220")
+                .registrationDate(LocalDateTime.of(2020, 5, 20, 0, 0))
                 .price(15000)
                 .fuelType(FuelType.DIESEL)
                 .mileage(60000)
@@ -22,12 +23,38 @@ public final class TestDataUtil {
                 .build();
     }
 
-    public static CarRequestDto createCarDtoRequest() {
+    public static CarRequestDto createCarDtoRequestBefore2015() {
 
         return CarRequestDto.builder()
                 .make("Toyota")
                 .model("Corolla")
                 .registrationDate(LocalDateTime.of(2014, 5, 20, 0, 0))
+                .price(15000)
+                .fuelType(FuelType.DIESEL)
+                .mileage(60000)
+                .transmission(Transmission.MANUAL)
+                .build();
+    }
+
+    public static CarRequestDto createCarRequestDto() {
+
+        return CarRequestDto.builder()
+                .make("Mercedes")
+                .model("C220")
+                .registrationDate(LocalDateTime.of(2020, 5, 20, 0, 0))
+                .price(15000)
+                .fuelType(FuelType.DIESEL)
+                .mileage(60000)
+                .transmission(Transmission.MANUAL)
+                .build();
+    }
+
+    public static CarResponseDto createCarResponseDto() {
+
+        return CarResponseDto.builder()
+                .make("Mercedes")
+                .model("C220")
+                .registrationDate(LocalDateTime.of(2020, 5, 20, 0, 0))
                 .price(15000)
                 .fuelType(FuelType.DIESEL)
                 .mileage(60000)
--- src/test/java/com/assignement/car_sales_garage/services/CarServiceTests.java
+++ src/test/java/com/assignement/car_sales_garage/services/CarServiceTests.java
@@ -3,8 +3,11 @@ package com.assignement.car_sales_garage.services;
 
 import com.assignement.car_sales_garage.TestDataUtil;
 import com.assignement.car_sales_garage.domain.dtos.CarRequestDto;
+import com.assignement.car_sales_garage.domain.dtos.CarResponseDto;
 import com.assignement.car_sales_garage.domain.entities.Car;
 import com.assignement.car_sales_garage.exceptions.InvalidCarException;
+import com.assignement.car_sales_garage.mapper.CarMapper;
+import com.assignement.car_sales_garage.repositories.CarRepository;
 import com.assignement.car_sales_garage.services.impl.CarServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,7 +15,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -21,14 +28,34 @@ public class CarServiceTests {
     @InjectMocks
     CarServiceImpl underTest;
 
+    @Mock
+    CarRepository carRepository;
+
+    @Mock
+    CarMapper carMapper;
+
     @Test
     public void whenRegisterCarBefore2015__thenRejectCar() {
 
-        CarRequestDto oldCar = TestDataUtil.createCarDtoRequest();
+        CarRequestDto oldCar = TestDataUtil.createCarDtoRequestBefore2015();
 
         assertThatThrownBy(() -> underTest.addCar(oldCar))
                 .isInstanceOf(InvalidCarException.class)
                 .hasMessageContaining("Car registered before 2015 is not allowed");
+    }
+
+    @Test
+    public void whenRegisterCarAfter2015__thenSaveCar() {
+        CarRequestDto carRequest = TestDataUtil.createCarRequestDto();
+        CarResponseDto carResponseDto = TestDataUtil.createCarResponseDto();
+        Car expected = TestDataUtil.createCarEntity();
+
+        when(carMapper.toEntity(any(CarRequestDto.class))).thenReturn(expected);
+        when(carMapper.toDto(any(Car.class))).thenReturn(carResponseDto);
+        when(carRepository.save(eq(expected))).thenReturn(expected);
+
+        assertThat(underTest.addCar(carRequest)).isEqualTo(carResponseDto);
+
     }
 
 
